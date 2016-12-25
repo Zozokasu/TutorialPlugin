eac87734b60bab6a42edc78d6050baf4fcfeec5c
BuildTools <unconfigured@null.spigotmc.org>
2016-12-26 03:02:42 +0900

    #

---
+++ src/main/java/kasu/test/plugin/tutorialplugin/Zokasu.java
@@ -0,0 +1,18 @@
+package kasu.test.plugin.tutorialplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Zokasu extends JavaPlugin {
+	
+	@Override
+	public void onEnable(){
+		getLogger().info("onEnableŒÄ‚Ño‚³‚ê");
+	
+	}
+	
+	@Override
+	public void onDisable(){
+		getLogger().info("onDisableŒÄ‚Ño‚³‚ê");
+		
+	}
+}
