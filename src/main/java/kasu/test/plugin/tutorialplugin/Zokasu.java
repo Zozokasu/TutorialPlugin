7a014b6d5d4f8211a52c43d020f551ad15ed6a7f
BuildTools <unconfigured@null.spigotmc.org>
2016-12-28 23:50:05 +0900

    練習、コマンド追加。

--- src/main/java/kasu/test/plugin/tutorialplugin/Zokasu.java
+++ src/main/java/kasu/test/plugin/tutorialplugin/Zokasu.java
@@ -1,18 +1,27 @@
 package kasu.test.plugin.tutorialplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.*;
+
 
 public class Zokasu extends JavaPlugin {
 	
 	@Override
 	public void onEnable(){
-		getLogger().info("onEnableŒÄ‚Ño‚³‚ê");
-	
+	    Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Test");
+	    System.out.println("Test");
 	}
-	
 	@Override
 	public void onDisable(){
-		getLogger().info("onDisableŒÄ‚Ño‚³‚ê");
-		
+	    System.out.println("test");
+	}
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+	    if(cmd.getName().equalsIgnoreCase("jf")){
+	        sender.sendMessage("jfa");
+	        return true;
+	    }
+	    return false;
 	}
 }
